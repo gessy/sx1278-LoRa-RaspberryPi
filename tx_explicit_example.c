20f2406a80df288bad962a5d4d002b47dfeadfa7
gessy <Alexander.Shadrin@emc.com>
2020-06-14 16:11:27 +0300

    update

--- tx_explicit_example.c
+++ tx_explicit_example.c
@@ -1,10 +1,10 @@
 #include "LoRa.h"
 
-void tx_f(txData *tx){
-    printf("tx done \n");
+void tx_f(txData *tx) {
+    printf("tx done test working \n");
 }
 
-int main(){
+int main() {
 
     char txbuf[255];
     LoRa_ctl modem;
@@ -13,22 +13,25 @@ int main(){
     modem.spiCS = 0;//Raspberry SPI CE pin number
     modem.tx.callback = tx_f;
     modem.tx.data.buf = txbuf;
-    memcpy(modem.tx.data.buf, "LoRa", 5);//copy data we'll sent to buffer
-    modem.tx.data.size = 5;//Payload len
-    modem.eth.preambleLen=6;
-    modem.eth.bw = BW62_5;//Bandwidth 62.5KHz
-    modem.eth.sf = SF12;//Spreading Factor 12
-    modem.eth.ecr = CR8;//Error coding rate CR4/8
-    modem.eth.CRC = 1;//Turn on CRC checking
-    modem.eth.freq = 434800000;// 434.8MHz
-    modem.eth.resetGpioN = 4;//GPIO4 on lora RESET pin
-    modem.eth.dio0GpioN = 17;//GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
-    modem.eth.outPower = OP20;//Output power
-    modem.eth.powerOutPin = PA_BOOST;//Power Amplifire pin
-    modem.eth.AGC = 1;//Auto Gain Control
-    modem.eth.OCP = 240;// 45 to 240 mA. 0 to turn off protection
-    modem.eth.implicitHeader = 0;//Explicit header mode
+    memcpy(modem.tx.data.buf, "Hello Sasha", 11);//copy data we'll sent to buffer
+    modem.tx.data.size = 11;//Payload len
+
+    modem.eth.freq = 434000000; // 434.0MHz
+    modem.eth.bw = BW125; //Bandwidth 62.5KHz
+    modem.eth.sf = SF9; //Spreading Factor 12
+    modem.eth.ecr = CR7; //Error coding rate CR4/8
     modem.eth.syncWord = 0x12;
+    modem.eth.outPower = OP17;//Output power
+    modem.eth.OCP = 100; // 45 to 240 mA. 0 to turn off protection
+    modem.eth.preambleLen = 8;
+    modem.eth.AGC = 1; //Auto Gain Control
+    modem.eth.resetGpioN = 22; //GPIO4 on lora RESET pin
+    modem.eth.dio0GpioN = 4; //GPIO17 on lora DIO0 pin to control Rxdone and Txdone interrupts
+
+    modem.eth.CRC = 0; //Turn on CRC checking
+    modem.eth.powerOutPin = PA_BOOST; //Power Amplifire pin
+    modem.eth.implicitHeader = 0; //Explicit header mode
+
     //For detail information about SF, Error Coding Rate, Explicit header, Bandwidth, AGC, Over current protection and other features refer to sx127x datasheet https://www.semtech.com/uploads/documents/DS_SX1276-7-8-9_W_APP_V5.pdf
 
     LoRa_begin(&modem);
@@ -38,8 +41,8 @@ int main(){
     printf("Tpkt: %f\n", modem.tx.data.Tpkt);
     printf("payloadSymbNb: %u\n", modem.tx.data.payloadSymbNb);
 
-    printf("sleep %d seconds to transmitt complete\n", (int)modem.tx.data.Tpkt/1000);
-    sleep(((int)modem.tx.data.Tpkt/1000)+1);
+    printf("sleep %d seconds to transmitt complete\n", (int) modem.tx.data.Tpkt / 1000);
+    sleep(((int) modem.tx.data.Tpkt / 1000) + 1);
 
     printf("end\n");
 
